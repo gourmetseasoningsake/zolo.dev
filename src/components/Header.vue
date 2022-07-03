6fe1ba0271c38256c1809d3992c02d86618a32f5
gourmetseasoningsake <jerome@shtill.com>
2022-07-03 14:05:39 +0200

    chore: setup dev env [wip]

---
+++ src/components/Header.vue
@@ -0,0 +1,16 @@
+<template>
+  <header>
+    {{ title }}
+  </header>
+</template>
+
+
+<script>
+  export default {
+    data() {
+      return {
+        title: "hello"
+      }
+    }
+  }
+</script>
