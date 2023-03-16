d61cce64039b184d9e2603ae54287c2d28913a01
gourmetseasoningsake <jerome@shtill.com>
2023-03-16 01:10:55 +0100

    dev: add component 2

--- src/components/GithubLogin.vue
+++ src/components/GithubLogin.vue
@@ -8,6 +8,8 @@
 </template>
 
 <script>
+import {uid} from "../utils/random";
+
 export default {
   props: {
     title: {
@@ -19,21 +21,55 @@ export default {
       required: true,
     },
   },
-  methods: {
-    loginOauthUrl() {
+  created() {
+    const urlParams = new URLSearchParams(window.location.search);
+    const localStateVar = window.sessionStorage.getItem(
+      "zolosake:github_state",
+    );
+
+    window.sessionStorage.removeItem("zolosake:github_state");
+    if (
+      localStateVar &&
+      urlParams.has("state") &&
+      urlParams.has("code") &&
+      localStateVar === urlParams.get("state")
+    ) {
       fetch(
         import.meta.env.PUBLIC_API_URL +
-          import.meta.env.PUBLIC_API_PATH_GITHUB_LOGIN_OAUTH_URL,
+          import.meta.env.PUBLIC_API_PATH_GITHUB_LOGIN_OAUTH_AUTHORIZE,
         {
           method: "POST",
           body: JSON.stringify({
+            code: urlParams.get("code"),
             redirectUri: window.location.origin + this.redirectPath,
           }),
         },
       )
         .then((resp) => resp.json())
         .then((data) => {
-          window.sessionStorage.setItem("zolosake:gh_state", data.state);
+          console.log(data);
+        })
+        .catch(console.log);
+    }
+  },
+  methods: {
+    loginOauthUrl() {
+      const state = uid();
+
+      window.sessionStorage.setItem("zolosake:github_state", state);
+      fetch(
+        import.meta.env.PUBLIC_API_URL +
+          import.meta.env.PUBLIC_API_PATH_GITHUB_LOGIN_OAUTH_URL,
+        {
+          method: "POST",
+          body: JSON.stringify({
+            state,
+            redirectUri: window.location.origin + this.redirectPath,
+          }),
+        },
+      )
+        .then((resp) => resp.json())
+        .then((data) => {
           window.location.assign(data.url);
         })
         .catch(console.log);
