0331c920ca396ab0fd9d72e65c0ab9998f121733
gourmetseasoningsake <jerome@shtill.com>
2023-04-14 09:58:14 +0200

    fix(Speak): use fallback pronounciation only

--- src/components/Speak.vue
+++ src/components/Speak.vue
@@ -52,27 +52,29 @@ export default defineComponent({
       return this.focusWithin || this.focus ? "0" : undefined;
     },
   },
-  mounted() {
-    const voices = speechSynthesis
-      ?.getVoices()
-      .filter(({lang}) => lang.startsWith(this.lang.split("-")[0]!))
-      .sort(({lang}) => (lang === this.lang ? -1 : 1));
-
-    if (voices?.[0]) {
-      const utter = new SpeechSynthesisUtterance(this.text);
-      utter.lang = voices[0].lang;
-      utter.voice = voices[0];
-      this.utter = utter;
-    }
-  },
+  // mounted() {
+  //   const voices = speechSynthesis
+  //     ?.getVoices()
+  //     .filter(({lang}) => lang.startsWith(this.lang.split("-")[0]!))
+  //     .sort(({lang}) => (lang === this.lang ? -1 : 1));
+  //
+  //   if (voices?.[0]) {
+  //     const utter = new SpeechSynthesisUtterance(this.text);
+  //     utter.lang = voices[0].lang;
+  //     utter.voice = voices[0];
+  //     this.utter = utter;
+  //   }
+  // },
   methods: {
     speak() {
-      if (this.utter) {
-        speechSynthesis.speak(this.utter);
-      } else {
-        const audio = new Audio(this.fallback);
-        audio.play();
-      }
+      // if (this.utter) {
+      //   speechSynthesis.speak(this.utter);
+      // } else {
+      //   const audio = new Audio(this.fallback);
+      //   audio.play();
+      // }
+      const audio = new Audio(this.fallback);
+      audio.play();
     },
   },
 });
